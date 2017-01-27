98dd380520fdd70762f2e9fb6a37b28ec7e18b59
Annie Shen <annieshenca@AnnieS-MBP.local>
2017-01-27 00:34:40 -0800

    pa2 wip

--- pa2/List.c
+++ pa2/List.c
@@ -5,37 +5,45 @@
  * List.c
  */
 
-include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-#include<assert.h>
-#include"Dictionary.h"
+#include"List.h"
 
-//private types -------------------------------------------------------
+//structs -------------------------------------------------------
 
-//NodeObj
+//Private NodeObj type
 typedef struct NodeObj{
-    char* key;
-    char* value;
+    int item;
     struct NodeObj* next;
+    struct NodeObj* prev;
 } NodeObj;
 
-//Node
+//Private Node type
 typedef NodeObj* Node;
 
+//Private ListObj type
+typedef struct ListObj{
+   Node head;
+   Node tail;
+   Node cursor;
+   int numItems;
+   int cIndex;
+} ListObj;
+
+//Constructors-Destructor ----------------------------------------
+
 //newNode()
-//constructor for Node type
-Node newNode(char* k, char* v) {
+//Returns reference to new Node object. Initializes item, next, and prev fields.
+Node newNode(int item) {
     Node N = malloc(sizeof(NodeObj));
-    assert(N!=NULL); //die if N equals to NULL
-    N->key = k;
-    N->value = v;
+    N->item = item;
     N->next = NULL;
+    N->prev = NULL;
     return(N);
 }
 
 //freeNode()
-//destructor for the Node type
+//Frees heap memory pointed to by *pN, sets *pN to NULL.
 void freeNode(Node* pN){
     if( pN!=NULL && *pN!=NULL ){
         free(*pN); //prevent memory leak
@@ -43,183 +51,333 @@ void freeNode(Node* pN){
     }
 }
 
-//DictionaryObj
-typedef struct DictionaryObj{
-   Node top;
-   int numItems;
-} DictionaryObj;
+//newList()
+//Returns reference to new empty List object.
+List newList(void){
+	List L = malloc(sizeof(ListObj));
+	L->head=NULL;
+	L->tail=NULL;
+	L->cursor=NULL;
+	L->numItems=0;
+	L->cIndex=-1;
 
-
-//public functions -----------------------------------------------
-
-//newDictionary()
-//constructor for the Dictionary type
-Dictionary newDictionary(void){
-   Dictionary D = malloc(sizeof(DictionaryObj));
-   assert(D!=NULL);
-   D->top = NULL;
-   D->numItems = 0;
-   return D;
+	return L;
 }
 
-//freeDictionary()
-//destructor for the Dictionary type
-void freeDictionary(Dictionary* pD){
-   if( pD!=NULL && *pD!=NULL ){
-       if( !isEmpty(*pD) ){
-           makeEmpty(*pD);
-       }
-       free(*pD);
-       *pD = NULL;
-   }
+//freeList()
+//Frees all heap memory associated with List *pL, and sets *pL to NULL.
+void freeList(List* pL){
+	if(pL != NULL && *pL != NULL){
+		while( length(*pL) != 0 ){
+			deleteBack(*pL);
+		}
+		free(*pL);
+		*pL=NULL;
+	}
 }
 
-//isEmpty()
-//returns 1 (true) if D is empty, 0 (false) otherwise
-int isEmpty(Dictionary D) {
-    if (D == NULL) { //when Dictionary is NULL
-        fprintf(stderr, "Dictionary Error: calling isEmpty() on NULL Dictionary reference\n");
-    } else if(D->numItems == 0) {
-        return 1; //return true because it is empty
-    } else{
-        return 0; //return false saying it is NOT empty
-    }
+//Access functions ---------------------------------------------
+
+//length()
+//Returns the number of elements in this List.
+int length(List L){
+	return L->numItems;
 }
 
-//size()
-//returns the size of (key,value) pair in D
-int size(Dictionary D){
-    return D->numItems;
+//index()
+//If cursor is defined, returns the index of the cursor element, otherwise returns -1.
+int index(List L){
+	if(L->cursor == NULL){
+		return -1; //set cursor index out of range
+	}
+	return L->cIndex;
 }
 
-//lookup()
-//return the value that is in D's (key, value), else return NULL if there's no such key
-char* lookup(Dictionary D, char* k){
-    Node N = D->top;
-
-    while (N != NULL){
-        if(strcmp(N->key, k) == 0){ //(=0) means true, (=1) equals false
-            //printf("lookup: %s\n",N->value);
-            return N->value;
-        }
-        N = N->next;
-    }
-    //printf("null\n");
-    return NULL; //return NULL if there's no such pair
+//front()
+//Returns front element. Pre: length()>0
+int front(List L){
+	if(length(L) <= 0){
+		printf("front() Error: List is empty");
+	}
+	return L->head->item;
 }
 
-//insert()
-//insert new (key, value) into D
-void insert(Dictionary D, char* k, char* v) {
-    //considering if D is empty or already had contents
-    Node N = newNode(k, v); //new Node
+//back()
+//Returns back element. Pre: length()>0
+int back(List L){
+	if(length(L) <= 0){
+		printf("back() Error: List is empty");
+	}
+	return L->tail->item;
+}
 
-    if (lookup(D, k) == NULL) { //the pair doesn't exist in D yet
-            N->next = D->top;
-            D->top = N;
-            D->numItems++;
-        }
-     else{ //the key is duplicate!
-        fprintf(stderr, "Dictionary error: cannot insert duplicate keys\n");
-        exit(EXIT_FAILURE);
-    }
-    //Forgot this is Stack, not Queue!
-//    Node top = D->top;
-//    Node new = newNode(k, v);
-//
-//    if(lookup(D, k) == NULL) { //if the pair doesn't exist
-//        if(top == NULL) { //if D is empty
-//            top = new;
-//        } else { //if D is NOT empty
-//            if(top->next != NULL) {
-//                top = top->next;
-//            }
-//            top->next = new;
-//        }
-//        D->numItems++; //now one more item
-//    } else{ //as in if the key existed
-//        fprintf(stdout, "Dictionary error: cannot insert duplicate keys");
-//    }
+//get()
+//Returns cursor element. Pre: length()>0, index()>=0
+int get(List L){
+	if(length(L) <= 0){
+		printf("get() Error: List is empty");
+	}
+	if(index(L) < 0){
+		printf("get() Error: invalid index");
+	}
+	return L->cursor->item;
+}
+
+//equals(List A, List B)
+//Returns true if A and B are the same integer sequence.
+//The cursor is ignored in both lists.
+int equals(List A, List B){
+	if(length(A) != length(B)){
+		return 0;
+	}
+	Node M = A->head;
+	Node N = B->head;
+	while(M != NULL && N != NULL){
+		if(M->item == N->item){
+			//If items match up, set next node as the new head and continue comparing
+			M = M->next;
+			N = N->next;
+		}else	return 0;
+	}
+	return 1;
+}
+
+//clear()
+//Resets this List to its original empty state.
+void clear(List L){
+	freeList(&L);
+	L->head = NULL;
+	L->tail = NULL;
+	L->cursor = NULL;
+	L->cIndex = -1;
+	L->numItems = 0;
+}
+
+//moveFront()
+//If List is non-empty, places the cursor under the front
+//element, otherwise does nothing.
+void moveFront(List L){
+	if(length(L) > 0){
+		L->cursor = L->head;
+		L->cIndex = 0;
+	}
+}
+
+//moveBack()
+//If List is non-empty, places the cursor under the back element,
+//otherwise does nothing.
+void moveBack(List L){
+	if(length(L) > 0){
+		L->cursor = L->tail;
+		L->cIndex = length(L) -1;
+	}
+}
+
+//movePrev()
+//If cursor is defined and not at front, moves cursor one step toward
+//front of this List, if cursor is defined and at front, cursor becomes
+//undefined, if cursor is undefined does nothing.
+void movePrev(List L){
+	if(L->cursor == L->head){
+		L->cursor = NULL;
+		L->cIndex = -1;
+	} else{
+		L->cursor = L->cursor->prev;
+		L->cIndex--;
+	}
+}
+
+//moveNext()
+//If cursor is defined and not at back, moves cursor one step toward
+//back of this List, if cursor is defined and at back, cursor becomes
+//undefined, if cursor is undefined does nothing.
+void moveNext(List L){
+	if(L->cursor != NULL){
+		if(L->cursor == L->tail){
+			L->cursor = NULL;
+			L->cIndex = -1;
+		} else{
+			L->cursor = L->cursor->next;
+			L->cIndex++;
+		}
+	}
+	//Do nothing if cursor is null.
+}
+
+//prepend(List L, int data)
+//Insert new element into this List. If List is non-empty,
+//insertion takes place before front element.
+void prepend(List L, int data){
+	Node A = newNode(data);
+	if(length(L) == 0){ //List is empty
+		L->head = A;
+		L->tail = A;
+	}
+	if(length(L) > 0){ //List is Not empty
+		A->next = L->head;
+		L->head->prev = A;
+		L->head = A;
+		if(L->cIndex != -1){
+			L->cIndex++;
+		}
+	}
+	L->numItems++; //increment number of items
+}
+
+//append(int data)
+//Insert new element into this List. If List is non-empty,
+//insertion takes place after back element.
+void append(List L, int data){
+	Node A = newNode(data);
+	if(length(L) == 0){ //List is empty
+		//System.out.println("reached here 1?");
+		L->head = A;
+		L->tail = A;
+	} else if(length(L) > 0){ //List is Not empty
+		L->tail->next = A; //Insert after tail
+		A->prev = L->tail;
+		L->tail = A;
+	}
+	L->numItems++;
+}
+
+//insertBefore(int data)
+//Insert new element before cursor.
+//Pre: length()>0, index()>=0
+void insertBefore(List L, int data){
+	if(length(L) <= 0){ //If the list is empty
+		printf("insertBefore() Error: List is empty");
+	}
+	if(index(L) < 0){ //If cursor index is invalid
+		printf("insertBefore() Error: invalid index");
+	}
+
+	Node A = newNode(data);
+	if(length(L) == 1){ //if cursor is at head and null is in front of the cursor
+		L->cursor->prev = A; //Set the null before the cursor to be the new node
+		A->next = L->cursor; //Set new node's next as the cursor
+	} else { // when length is more than 1, that cursor.prev is NOT a null
+		Node temp = L->cursor->prev; //Save current cursor.prev as a new node named temp
+		L->cursor->prev = A;
+		A->next = L->cursor;
+		if(temp != NULL){
+			temp->next = A; //The old cursor.prev now is in front of node A
+			A->prev = temp;
+		}
+		if(L->cursor == L->head){
+			L->head = A;
+		}
+	}
+	L->cIndex++;
+	L->numItems++;
+}
+
+//insertAfter(int data)
+//Inserts new element after cursor.
+//Pre: length()>0, index()>=0
+void insertAfter(List L, int data){
+	if(length(L) <= 0){
+		printf("insertAfter() Error: List is empty");
+	}
+	if(index(L) < 0){
+		printf("insertAfter() Error: invalid index");
+	}
+
+	Node A = newNode(data);
+	if(length(L) == 1){ //if cursor is at head and null is after the cursor
+		L->cursor->next = A;
+		A->prev = L->cursor;
+	} else { // when length is more than 1, that cursor.prev is NOT a null
+		Node temp = L->cursor->next; //Save current cursor.prev as a new node named temp
+		L->cursor->next = A;
+		A->prev = L->cursor;
+		temp->prev = A; //The old cursor.prev now is in front of node A
+		A->next = temp;
+	}
+	L->numItems++;
+}
+
+//deleteFront()
+//Deletes the front element. Pre: length()>0
+void deleteFront(List L){
+	if(length(L) <= 0){
+		printf("deleteFront() Error: List is empty");
+	}
+	if(length(L) == 1){ //If the list has only one element
+		L->head = L->tail = L->cursor = NULL; //Reset
+		L->cIndex = -1;
+	} else{ //The list has more than 1 element
+		L->head = L->head->next;
+		L->head->prev = NULL;
+		L->numItems--;
+	}
+}
+
+//deleteBack()
+//Deletes the back element. Pre: length()>0
+void deleteBack(List L){
+	if(length(L) <= 0){
+		printf("deleteBack() Error: List is empty");
+	}
+	if(length(L) == 1){ //If the list has only one element
+		L->head = L->tail = L->cursor = NULL; //Reset
+		L->cIndex = -1;
+	} else{ //The list has more than 1 element
+		L->tail = L->tail->prev;
+		L->tail->next = NULL;
+		L->numItems--;
+	}
 }
 
 //delete()
-//delete k key from D's (key, value)
-void delete(Dictionary D, char* k){
-    Node N = D->top;
-
-    if(lookup(D, k) == NULL){
-        fprintf(stderr, "Dictionary error: cannot delete non-existing keys\n");
-        exit(EXIT_FAILURE);
-    }
-
-    //If the key is found! then:
-    if(strcmp(N->key, k) == 0){ //if the first node matches the key
-        D->top = N->next;
-        freeNode(&N); //prevent memory leak
-        N = NULL; //initialize
-        D->numItems--;
-
-    } else{ //if it's after the first node
-        //int found = 0;
-        while(N != NULL && N->next != NULL){
-            if(strcmp(N->next->key, k) == 0){//(=0) means true, (=1) equals false
-                Node G = N;
-                Node C = N->next;
-                G->next = C->next;
-                N = G;
-                freeNode(&C);
-                //Node temp = N->next; //set M as N
-                //N->next = N->next->next; //set temp as N's next key
-                //freeNode(&temp); //discard temp
-                //temp = NULL;
-                D->numItems--;
-                //found =1;
-            }
-            N = N->next; //loop until key is found
-        }
-
-    }
+//Deletes cursor element, making cursor undefined.
+//Pre: length()>0, index()>=0
+void delete(List L){
+	if(length(L) <= 0){
+		printf("delete() Error: List is empty");
+	}
+	if(index(L) < 0){
+		printf("delete() error: invalid index");
+	}
+	if(L->cIndex == 0){ //If cursor is at head
+		deleteFront(L); //Call deleteFront() to do the same job
+	} else if(L->cIndex == length(L) -1){ //If the cursor is at tail
+		deleteBack(L); //Call deleteBack() to do the same job
+	} else{ //If the cursor is in the middle of the list
+		Node before = L->cursor->prev;
+		Node after = L->cursor->next;
+		before->next = after;
+		after->prev = before;
+		L->cursor = NULL;
+		L->cIndex = -1;
+		L->numItems--;
+	}
 }
 
-void empty(Node N){
-    if(N != NULL){
-        empty(N->next);
-        freeNode(&N);
-    }
+//void printList(FILE* out, List L);
+
+
+//Returns a new List representing the same integer sequence as this
+//List. The cursor in the new list is undefined, regardless of the
+//state of the cursor in this List. This List is unchanged.
+List copyList(List L){
+	List cp = newList();
+	Node temp = L->head;
+	while(temp != NULL){
+		append(cp, temp->item);
+		temp = temp->next;
+	}
+	return cp;
 }
 
-//makeEmpty()
-//resets-empty Dictionary D
-void makeEmpty(Dictionary D){
-    empty(D->top);
-    D->top = NULL;
-    D->numItems = 0;
-}
+////Returns a new List which is the concatenation of this list followed
+////by L. The cursor in the new List is undefined, regardless of the
+////states of the cursors in this List and L. The states of this List
+////and L are unchanged.
+//List concatList(List A, List B);
 
-//printRec()
-//additional help for printDictionary() to print recursively
-void printRec(FILE* out, Node N){
-    //printf("in printRec, outside of while loop");
-    if(N != NULL) {
-        printRec(out, N->next);
-        //printf("in printRec");
-        fprintf(out, "%s %s\n", N->key, N->value);
-    }
-}
 
-//printDictionary()
-//prints a text representation of D to the file pointed to by out
-void printDictionary(FILE* out, Dictionary D){
-    //Node N;
 
-    if(D == NULL){
-        fprintf(stderr, "Dictionary Error: calling printDictionary() on NULL Dictionary reference\n");
-        exit(EXIT_FAILURE);
-    }
-    //printf("in printDictionary, in loop before calling printRec\n");
-    printRec(out, D->top);
 
-//    for(N=D->top; N!=NULL; N=N->next){
-//        fprintf(out, "%s %s\n", N->key, N->value);
-//    }
-}
+
+
--- pa2/ListClient.c
+++ pa2/ListClient.c
@@ -5,7 +5,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-#include"List.h"
+#include "List.h"
 
 int main(int argc, char* argv[]){
    
