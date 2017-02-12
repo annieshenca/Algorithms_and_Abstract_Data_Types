1920f4e69eef720ad4654782096415db3a68205a
Annie Shen <annieshenca@eduroam-169-233-241-150.ucsc.edu>
2017-02-12 00:05:13 -0800

    update Matrix.java

--- pa3/Matrix.java
+++ pa3/Matrix.java
@@ -98,22 +98,30 @@ public class Matrix{
 	//makeZero()
 	//Sets this Matrix to the zero state
 	void makeZero(){
-		for(int l=1; l <= getSize(); l++){
-			matrix[l] = null;
+		for(int i = 1; i <= getSize(); i++){
+			matrix[i] = new List();
 		}
+		
 	}
 	
 	//Matrix copy()
 	//Returns a new Matrix having the same entries as this Matrix
-//	Matrix copy(){
-//		Matrix copyM = new Matrix(getSize());
-//		//List[] copy = new List[getSize()];
-//		for(int i = 0; i < getSize(); i++){
-//			copyM[i] = matrix[i];
-//		}
-//		return copyM;
-//	}
-//	
+	Matrix copy(){
+		Matrix copy = new Matrix(getSize());
+
+		for(int i = 1; i <= getSize(); i++){
+			matrix[i].moveFront();
+			while( matrix[i].index() != -1){
+				
+				int a = ((Entry)matrix[i].get()).column;
+				double b =((Entry)matrix[i].get()).data;
+				copy.changeEntry(i, a, b);
+				matrix[i].moveNext();
+			}
+		}
+		return copy;
+	}
+	
 	//changeEntry(int i, int j, double x)
 	//Changes ith row, jth column of this Matrix to x
 	//Pre: 1<=i<=getSize(), 1<=j<=getSize()
@@ -137,22 +145,44 @@ public class Matrix{
 					//while the matrix cursor is not invalid(-1), and the matrix's ith is less than j
 					matrix[i].moveNext();
 				}
-				if(matrix[i].index() == -1){
+				if(matrix[i].index() == -1){ //If cursor fall off the list
 					matrix[i].append(new Entry(j,x));
 				} else{
 					matrix[i].insertBefore(new Entry(j,x));
 				}
-				NNZ++;
+				
 			}
+			NNZ++;
 		}
 	}
-//	
-//	//Matrix scalarMult(double x)
-//	//Returns a new Matrix that is the scalar product of this Matrix with x
-//	Matrix scalarMult(double x){
-//		
+	
+	//private double dot(List A, List B)
+	//Computes the vector dot product of two matrix rows of List A and B
+//	private double dot(List A, List B){
+//		for(int n =1; n <= getSize(); n++){
+//			
+//			
+//			
+//		}
 //	}
-//	
+
+	//Matrix scalarMult(double x)
+	//Returns a new Matrix that is the scalar product of this Matrix with x
+	Matrix scalarMult(double x){
+		Matrix M = this.copy();
+		
+		for(int i=1; i <= getSize(); i++){
+			M.matrix[i].moveFront();
+			while(M.matrix[i].index() != -1){
+				double a = ((Entry)M.matrix[i].get()).data;
+				a = a * x;
+				((Entry)M.matrix[i].get()).data = a;
+				M.matrix[i].moveNext();
+			}
+		}
+		return M;
+	}
+
 //	//Matrix add(Matrix M)
 //	//Returns a new Matrix that is the sum of this Matrix with M
 //	//Pre: getSize()==M.getSize()
@@ -166,13 +196,56 @@ public class Matrix{
 //	Matrix sub(Matrix M){
 //		
 //	}
-//	
-//	//Matrix transpose()
-//	//Returns a new Matrix that is the transpose of this Matrix
-//	Matrix transpose(){
-//		
-//	}
-//	
+	
+	//Matrix transpose()
+	//Returns a new Matrix that is the transpose of this Matrix
+	Matrix transpose()
+	{
+		Matrix trans = new Matrix(this.getSize());
+		int i = 1;
+		int j = 1; //Start i at 1 for trans
+		double x;
+
+		for(i=1; i <= getSize(); i++) //j for copy
+		{
+			this.matrix[i].moveFront(); //Start copy's list i's cursor at front
+			 //Move copy at list j 's cursor to the front
+			j=1;
+			while(this.matrix[i].index() != -1) //while there's nodes to be read in copy's list j
+			{
+				x = ((Entry)this.matrix[i].get()).data;
+				trans.changeEntry(j, i, x);
+				j++;
+				this.matrix[i].moveNext();
+			}
+		}
+		return trans;
+//		copy.matrix[i].moveFront();
+//		//System.out.println("I'm here");
+//		//System.out.println(trans.matrix[i].index);
+//		while(copy.matrix[i] != null){
+//			for(int j = 1; j <= getSize(); j++){
+//				//System.out.println("and then I'm here");
+//				trans.matrix[j].moveFront();
+//				//System.out.println("and then I'm here");
+//				System.out.println(trans.matrix[j].index);
+//				//int aColumn = ((Entry)copy.matrix[i].get()).column;
+//				double aData = ((Entry)copy.matrix[i].get()).data;
+//			
+//				//int bColumn = ((Entry)trans.matrix[j].get()).column;
+//				double bData = ((Entry)trans.matrix[j].get()).data;
+//			
+//				bData = aData;
+//				((Entry)trans.matrix[j].get()).data = bData;
+//			
+//				copy.matrix[i].moveNext();
+//			
+//				//((Entry)M.matrix[i].get()).data = a;
+//				//M.matrix[i].moveNext();
+//			}
+//		}
+	}
+	
 //	//Matrix mult(Matrix M)
 //	//Returns a new Matrix that is the product of this Matrix with M
 //	//Pre: getSize()==M.getSize()
@@ -184,7 +257,7 @@ public class Matrix{
 	//toString()
 	//Overrides Object's toString() method
 	public String toString(){
-		String str = new String();
+		String str = "";
 		for(int i = 1; i<=getSize(); i++){
 			if(matrix[i].length() >0){
 				str += (i + ": " + matrix[i] + "\n");
