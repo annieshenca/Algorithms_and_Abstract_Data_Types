cce1c60b2ddb7d4b6e11c23ffd2b100ed4bb55b9
Annie Shen <annieshenca@eduroam-169-233-196-27.ucsc.edu>
2017-02-27 19:06:23 -0800

    fixed printList

---
+++ pa4/Lex.c
@@ -0,0 +1,37 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include "List.h"
+#include "Test.h"
+int main(){
+	FILE* out = stdout;
+/*	out = fopen("Test_Clear.out", "w");
+	if( out==NULL ){
+		printf("Unable to write to file\n");
+		exit(EXIT_FAILURE);
+	}*/
+	List a = newList();
+	append(a, 3);
+	printList(out, a);//3
+	clear(a);
+	printList(out, a);//
+
+	List b = newList();
+	prepend(b, 6);
+	printList(out, b);//6
+	prepend(b, 12);
+	printList(out, b);//12 6
+	clear(b);
+	printList(out, b);//
+
+	List c = newList();
+	append(c, 5);
+	printList(out, c);//5
+
+	fclose(out);
+	freeList(&b);
+	freeList(&a);
+	freeList(&c);
+return 0;
+}
+
